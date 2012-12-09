68f31c1fd87c79992f8d4c487089343e18887c54
Craig Wickesser <craig@mindscratch.org>
2012-12-08 22:35:23 -0500

    printing routes

--- lib/backbone_routing.rb
+++ lib/backbone_routing.rb
@@ -3,12 +3,26 @@ require 'rhino'
 require "backbone_routing/version"
 
 module BackboneRouting
-  def self.call
-    Rhino::Context.open do |context|
-      context.load "vendor/assets/javascripts/underscore.js"
-      context.load "vendor/assets/javascripts/backbone.js"
+  def self.load_sources(files, context)
+    files.each do |file|
+      puts "context: #{context}, file=#{file}"
+      context.load file
+    end
+  end
 
+  def self.call(cache_context=false)
+    process = ->(context) do
+      vendor_path = File.join('vendor', 'assets', 'javascripts')
+      assets = [File.join(vendor_path, 'underscore.js'), File.join(vendor_path, 'backbone.js')]
+      assets += [File.join('app', 'assets', 'javascripts', 'app.js')]
+      self.load_sources assets, context
       yield context
+    end
+
+    if cache_context
+      self.ctx ||= Rhino::Context.open { |context| process.call(context) }
+    else
+      self.ctx = Rhino::Context.open { |context| process.call(context) }
     end
   end
 
@@ -21,11 +35,9 @@ module BackboneRouting
   end
 end
 
-BackboneRouting.ctx = BackboneRouting.call do |ctx|
-  ctx.load 'app/assets/javascripts/app.js'
+BackboneRouting.call do |ctx|
   ctx['book'] = ctx.eval 'new Book({id: 593});'
   ctx['router'] = ctx.eval 'new AppRouter();'
 
   puts "Routes:", ctx.eval('router.routes')
-  ctx
 end
